c6c88e5bfd51c65c54fbe6554585d5fd63ea4a9d
cathrn3 <cawu@g.hmc.edu>
2020-10-06 17:34:54 -0700

    updated display image

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
+#import cloudinary as Cloud
+#from cloudinary.uploader import upload
+#from cloudinary.utils import cloudinary_url
 
 UPLOAD_FOLDER = 'static/uploads/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
@@ -8,6 +11,7 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "secret key"
+
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -32,29 +36,8 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('display_image',
-                                    filename=filename))
+            full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            return render_template("index.html", user_image = full_path)
         else:
             flash('Invalid image type')
             return redirect(request.url)
-    #return '''
-    #<!doctype html>
-    #<title>Upload new File</title>
-    #<h1>Art Style Identifier</h1>
-    #<p> Upload your image! (accepts .png, .jpg, .jpeg) </p>
-    #<form method=post enctype=multipart/form-data>
-      #<input type=file name=file>
-      #<input type=submit value=Upload>
-    #</form>
-    #'''
-#from flask import send_from_directory
-
-#@app.route('/uploads/<filename>')
-#def uploaded_file(filename):
-    #return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-@app.route('/display/<filename>')
-def display_image(filename):
-	#print('display_image filename: ' + filename)
-	return redirect(url_for('static', filename='uploads/' + filename), code=301)
