7b7899dec10b5bb9a3315df764a138b9d972937b
cathrn3 <cawu@g.hmc.edu>
2020-10-31 19:23:20 -0700

    heroku test

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ UPLOAD_FOLDER = 'static/uploads/'
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 app.secret_key = "secret key"
 
 
@@ -43,22 +44,22 @@ def upload_form():
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
+        # sphagetti code alert: this renders but really should be redirect, but there are issues with scrolling with redirect
         if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+            # doesn't work, suppose to display error message after go
+            return redirect(request.file)
         if file and not allowed_file(file.filename):
-            flash('Error: Invalid image type')
-            return redirect(request.url)
+            return render_template("upload.html", wrongext = 1, scroll = "display")
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            classification = predict_single(full_path)
-            return render_template("upload.html", user_image = full_path, classify = classification, scroll = "display")
+            #classification = predict_single(full_path)
+            return render_template("upload.html", user_image = full_path, scroll = "display")
+
+@app.errorhandler(413)
+def too_large(e): 
+    return render_template("upload.html", toobig = 1, scroll = "display"), 413
