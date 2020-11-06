711f5714e02337e21194bb2ebb58a9df2680260c
cathrn3 <cawu@g.hmc.edu>
2020-11-06 10:58:24 -0800

    working prediction

--- app.py
+++ app.py
@@ -2,44 +2,58 @@ import os
 from flask import Flask, flash, request, redirect, url_for, render_template, jsonify
 from werkzeug.utils import secure_filename
 from PIL import Image
-#from fastai.basic_train import load_learner
-#from fastai.vision import open_image
+from fastai.basic_train import load_learner
+from fastai.vision import open_image
+from fastai.vision import *
+import torchvision.transforms as T
 
 app = Flask(__name__)
 app.secret_key = "secret key"
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+#app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 
 
-#
-#learn = load_learner(path='./models', file='trained_model.pkl')
-#classes = learn.data.classes
+learn = load_learner(path='./models', file='trained_model.pkl')
+classes = learn.data.classes
 
 def predict_single(img_file):
     'function to take image and return prediction'
-    prediction = learn.predict(open_image(img_file))
+    prediction = learn.predict(img_file)
     probs_list = prediction[2].numpy()
     category = classes[prediction[1].item()]
     print("Prediction: %s" % category)
     return category
     
 
-@app.route('/')
-def upload_form():
-	return render_template('upload.html')
+@app.route("/")
+@app.route("/upload")
+@app.route("/upload.html")
+def upload():
+    """Redirect to where they should go"""
+    return redirect("/static/upload.html")
 
 
 # for uploading an image
 # NEEDS WORK: this function and classifications doesn't work for new updated upload and display that works on heroku
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        file = request.files['file']
+        file = request.files["file"]
         if file.filename == '':
             return redirect(request.file)
         if file and not allowed_file(file.filename):
-            return redirect(request.file)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            classification = predict_single(full_path)
-            return render_template("upload.html", classify = classification)
+            return render_template("upload.html", wrongext = 1, scroll = "display")
+        if file:
+            #placeholder stuff
+            #image = Image.open(file)
+            #image.show()
+            img_pil = PIL.Image.open(file)
+            img_tensor = T.ToTensor()(img_pil)
+            image = Image(img_tensor)
+            return predict_single(image)
