6eb38c7a9b32edbc9f4e13508404f28105e2c33d
cathrn3 <cawu@g.hmc.edu>
2020-11-09 11:58:48 -0800

    cleaned up code

--- app.py
+++ app.py
@@ -14,33 +14,28 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 @app.route("/upload")
 @app.route("/upload.html")
 def upload():
-    """Redirect to where they should go"""
     return redirect("/static/upload.html")
 
-
-# for uploading an image
-# NEEDS WORK: this function and classifications doesn't work for new updated upload and display that works on heroku
-
+# check if correct extension
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
+# upload and classify image
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files["image"]
-        """
-        if file.filename == '':
-            return redirect(request.file)
         if file and not allowed_file(file.filename):
-            return render_template("upload.html", wrongext = 1, scroll = "display")
-        if file:"""
-        img_pil = PIL.Image.open(file)
-        img_tensor = T.ToTensor()(img_pil)
-        image = Image(img_tensor)
-        classify = predict_single(image)
-        return predict_single(image)
+            return "Please upload a .jpg, .jpeg or .png"
+        else:
+            # convert pillow image to fastai recognizable image
+            img_pil = PIL.Image.open(file)
+            img_tensor = T.ToTensor()(img_pil)
+            image = Image(img_tensor)
+            classify = predict_single(image)
+            return predict_single(image)
+
 
 learn = load_learner(path='./models', file='trained_model.pkl')
 classes = learn.data.classes
@@ -50,5 +45,5 @@ def predict_single(img_file):
     prediction = learn.predict(img_file)
     probs_list = prediction[2].numpy()
     category = classes[prediction[1].item()]
-    print("Prediction: %s" % category)
+    #NEEDSWORK: print top 3 matches and percentages
     return category
