0098cae944ed149d1efc3dd39f3c696f6212e564
Henley <48139631+hsartin@users.noreply.github.com>
2020-11-13 14:59:27 -0500

    pylint changes
    pylint changes - need to add appropriate import statements to this file

--- functions/artist_predictor.py
+++ functions/artist_predictor.py
@@ -1,10 +1,11 @@
+"""This script contains functions and the model for the artist. Similar to predict_single in app.py."""
 # includes functions and model name for artist
 # mostly copied from predict_single in app.py in Catherine's branch
 
 # back end model, outputs prediction of given image
-artist_model = 'artist-stage-3.pkl' # 61.3% accuracy and classifies 53 artists
-artist_learn = load_learner(path='./models', file=artist_model)
-artist_classes = artist_learn.data.classes
+ARTIST_MODEL = 'artist-stage-3.pkl' # 61.3% accuracy and classifies 53 artists
+ARTIST_LEARN = load_learner(path='./models', file=ARTIST_MODEL)
+ARTIST_CLASSES = ARTIST_LEARN.data.classes
 
 # possible idea 1: pass in the learner
 # predict_single('img_file.jpg', artist_learn, artist_classes)
@@ -20,9 +21,9 @@ def predict_single(img_file, learn, classes):
 # possible idea 2: have a separate function for artist
 def artist_predict_single(img_file):
     'function to take image and return prediction'
-    prediction = artist_learn.predict(open_image(img_file))
+    prediction = ARTIST_LEARN.predict(open_image(img_file))
     probs_list = prediction[2].numpy()
     return {
-        'category': artist_classes[prediction[1].item()],
-        'probs': {c: round(float(probs_list[i]), 5) for (i, c) in enumerate(artist_classes)}
+        'category': ARTIST_CLASSES[prediction[1].item()],
+        'probs': {c: round(float(probs_list[i]), 5) for (i, c) in enumerate(ARTIST_CLASSES)}
     }
