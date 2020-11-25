504eecf2d8de40cafbbf60ffa90b0ef43e1653d3
avinella <avinella@g.hmc.edu>
2020-11-24 17:49:53 -0700

    added images for similar images

--- functions/clean_data.py
+++ functions/clean_data.py
@@ -1,3 +1,7 @@
+'''
+    Checks that image file sizes are larger than 1 MB (max for Github API)
+    Deletes files if too large
+'''
 from pathlib import Path
 import os
 
@@ -9,7 +13,6 @@ for root, dirs, files in os.walk(data_dir):
         print(root + ": " + str(num))
     for f in files:
         size = Path(os.path.join(root, f)).stat().st_size
-        # if size > 1000000:
-        #     print(f)
-        #     os.remove(os.path.join(root, f))
-        #
+        if size > 1000000:
+            os.remove(os.path.join(root, f))
+        
